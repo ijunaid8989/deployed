a7c4e6185e952018558383a71f9a2f8c8db17fa8
Junaid Farooq <junaid@evercam.io>
2019-12-08 08:14:25 +0000

    Added deploy model.

---
+++ server/API/models/deployModel.js
@@ -0,0 +1,28 @@
+'use strict';
+var mongoose         = require('mongoose'),
+    Schema           = mongoose.Schema;
+
+var deploySchema = new Schema({
+  branch: {
+    type: String,
+    required: [true, 'Branch cannot be blank.']
+  },
+  sha: {
+    type: String,
+    required: [true, 'Sha cannot be blank.']
+  },
+  user: {
+    type: String,
+    required: [true, 'User cannot be blank.']
+  },
+  commitDetails: {
+    type: String,
+    required: [true, 'Commit Details cannot be blank.']
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Deploys', deploySchema);
